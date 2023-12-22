01688caf08bd0eafcf6f43842df54a8a125eb09f
TekyRey <mwakabayah@gmail.com>
2023-12-22 16:51:16 +0300

    Fix build errors

--- src/components/organs/HeroSection.tsx
+++ src/components/organs/HeroSection.tsx
@@ -10,7 +10,7 @@ import { Link } from "react-router-dom";
 const HeroSection = () => {
   // const navigate = useNavigate();
     return (
-      <section className="w-full lg:h-screen md:h-[550px] h-[830px] relative overflow-x-hidden flex justify-end">
+      <section className="w-full lg:h-screen md:h-[550px] h-[830px] relative flex justify-end">
         {/* <Image className="h-[60%] w-[80%] lg:h-[90vh] md:h-[50vh] lg:w-1/2 md:w-[55%]" image={bgImage} alt="Hero Background Vector" /> */}
         <main className="w-full lg:h-full h-auto grid md:grid-cols-2 absolute top-0 left-0 lg:px-24 md:px-8 px-5 pt-24 md:pt-32 lg:pt-0">
           <div className="flex flex-col justify-center md:gap-6 gap-3 md:order-1 order-2">
@@ -22,7 +22,7 @@ const HeroSection = () => {
             </Text>
             <Text
               as="h1"
-              className=" text-color3 lg:text-7xl md:text-5xl text-3xl font-medium"
+              className=" text-color3 lg:text-4xl md:text-5xl text-3xl font-medium"
             >
               <Fade>{HeroTexts.secondText}</Fade>
             </Text>
--- src/components/pages/PlantTree.tsx
+++ src/components/pages/PlantTree.tsx
@@ -1,8 +1,5 @@
 import React from 'react';
 import Orders from '../atoms/Table';
-// import only datatable css from react bootstrap
-// import 'react-bootstrap-table-next/dist/react-bootstrap-table2.min.css';
-// import "react-bootstrap-table-next/dist/react-bootstrap-table2.min.css";
 
 const PlantTree: React.FC = () => {
   return (
